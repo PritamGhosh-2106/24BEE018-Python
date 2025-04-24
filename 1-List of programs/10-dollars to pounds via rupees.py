e6eba2cfafd8545a823218577804960cded4f54a
Pritam Prabir Ghosh <pritampghosh2006@gmail.com>
2025-04-24 22:46:44 +0530

    Add files via upload

---
+++ 1-List of programs/10-dollars to pounds via rupees.py
@@ -0,0 +1,4 @@
+a=int(input("Enter dollars"))
+b=a*48
+c=b/70
+print("dollars to pounds is",c,"pounds")
---
+++ 1-List of programs/14-celcius to fehrenheit.py
@@ -0,0 +1,3 @@
+a=int(input("Enter celcius"))
+b=(9/5*a)+32
+print("Celcius to fahrenheit is",b,"F")
