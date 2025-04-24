2bf6ab46c041e35d04e828446f7a4a2e3bcd61fc
Pritam Prabir Ghosh <pritampghosh2006@gmail.com>
2025-04-24 22:50:15 +0530

    Add files via upload

---
+++ 5-lists/5.py
@@ -0,0 +1,6 @@
+string_list = ["hello", "world", "python", "programming", "is fun"]
+
+uppercase_list = [string.upper() for string in string_list]
+
+print("Original list:", string_list)
+print("List with uppercase strings:", uppercase_list)
